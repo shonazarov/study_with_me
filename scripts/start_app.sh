c017400ffa0bc25e602ff82ecdb384ff6d9530ee
Zokir <example@gmail.com>
2023-08-19 23:22:45 +0100

    all deployment files fixed with IP adress

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/studyWithMe/studybud/settings.py
+sed -i 's/\[]/\["18.212.141.238"]/' /home/ubuntu/studyWithMe/studybud/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
