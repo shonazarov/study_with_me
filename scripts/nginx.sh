84c66cb80f458833b1fa5f5a0b3e158945766b85
Zokir <example@gmail.com>
2023-08-19 01:16:08 +0100

    Renamed project folder studyWithMe

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/studybud/nginx/nginx.conf /etc/nginx/sites-available/studybud
+sudo cp /home/ubuntu/studyWithMe/nginx/nginx.conf /etc/nginx/sites-available/studybud
 sudo ln -s /etc/nginx/sites-available/studybud /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -2,4 +2,4 @@
 
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
-pip install -r /home/ubuntu/studybud/requirements.txt
+pip install -r /home/ubuntu/studyWithMe/requirements.txt
